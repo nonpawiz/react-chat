72073e2b494616482a30413597fa77bbdccc5e0c
unknown <nonpawit.dev@gmail.com>
2023-07-05 16:24:39 +0700

    input below

--- src/views/auth/Login.tsx
+++ src/views/auth/Login.tsx
@@ -1,100 +1,120 @@
 import { useState } from "react";
 import axios from "axios";
 import { login, logout } from "../../redux/userSlice";
-import store from '../../redux/store'
-import Meta from "../../layouts/Meta";
-
+import store from "../../redux/store";
+import Meta from "../../Layouts/Meta";
 
 function Login() {
+  const [name, setName] = useState("admin");
+  const [pass, setPass] = useState("admin12345");
 
-    const [name, setName] = useState('admin');
-    const [pass, setPass] = useState('admin12345');
+  function auth() {
+    if (name == "" && pass == "") {
+      alert("Enter the Firstname & Password");
+    } else if (name == "") {
+      alert("Enter the Firstname");
+    } else if (pass == "") {
+      alert("Enter the Password");
+    } else {
+      const formData = new FormData();
+      formData.append("username", name);
+      formData.append("password", pass);
 
-    function auth() {
-        if (name == '' && pass == '') {
-            alert('Enter the Firstname & Password')
-        } else if (name == '') {
-            alert('Enter the Firstname')
-        } else if (pass == '') {
-            alert('Enter the Password')
-        }
-        else {
+      const option = {
+        method: "post",
+        url: "https://bem.services/jwt_login",
+        data: formData,
+      };
 
-            var formData = new FormData();
-            formData.append('username', name);
-            formData.append('password', pass);
-
-            let option = {
-                method: 'post',
-                url: 'https://bem.services/jwt_login',
-                data: formData
-            }
-
-            const res = axios(option)
-                .then(function (response) {
-                    // console.log(response.data.data.name);
-                    // setUser(response.data.data)
-                    let data = response.data.data;
-                    if (data.name) {
-                        store.dispatch(login({
-                            username: data.name,
-                            role: data.role,
-                            token: data.token,
-                            loggedIn: true
-                        }))
-                        setTimeout(() => {
-                            store.dispatch(logout());
-                        }, 86400000) //24ชม. เตะออก
-                    } else {
-                        alert(data[1])
-                    }
-
-                })
-                .catch(function (error) {
-                    alert('Server not Responding');
-                });
-
-
-
-        }
-
+      axios(option)
+        .then(function (response) {
+          // console.log(response.data.data.name);
+          // setUser(response.data.data)
+          const data = response.data.data;
+          if (data.name) {
+            store.dispatch(
+              login({
+                username: data.name,
+                role: data.role,
+                token: data.token,
+                loggedIn: true,
+              })
+            );
+            setTimeout(() => {
+              store.dispatch(logout());
+            }, 86400000); //24ชม. เตะออก
+          } else {
+            alert(data[1]);
+          }
+        })
+        .catch(function (error) {
+          alert("Server not Responding");
+          console.log(error);
+        });
     }
+  }
 
-    return (
-        <>
-            <Meta title='Login' description='Login system' name='Consult SIIT' type='article' />
-            <div className="d-flex align-items-center justify-content-center" style={{ height: '100vh' }}>
-                <div className="card border-0">
-                    <div className="card-body">
-                        <div className="text-center mb-3">
-                            <img src="/nonpawiz.png" style={{ width: '50px' }} alt="" />
-                        </div>
-                        <div className="text-center mb-3">
-                            <span className="text-main fw-normal">เข้าสู่ระบบ</span>
-                        </div>
-                        <div className="row">
-                            <div className="col-md-12 mb-3">
-                                <div className="form-floating mb-3">
-                                    <input type="fname" className="form-control" id="name" placeholder="ชื่อผู้ใช้" onChange={(e) => setName(e.target.value)} value={name} />
-                                    <label htmlFor="floatingInput">ชื่อผู้ใช้</label>
-                                </div>
-                            </div>
-                            <div className="col-md-12 mb-3">
-                                <div className="form-floating mb-3">
-                                    <input type="password" className="form-control" id="pass" placeholder="รหัสผ่าน" onChange={(e) => setPass(e.target.value)} value={pass} />
-                                    <label htmlFor="floatingPassword">รหัสผ่าน</label>
-                                </div>
-                            </div>
-                            <div className="col-md-12">
-                                <button className="btn bg-main border-0 text-light p-2 fs-16 w-100 fw-normal" onClick={auth}>เข้าสู่ระบบ</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
+  return (
+    <>
+      <Meta
+        title="Login"
+        description="Login system"
+        name="Consult SIIT"
+        type="article"
+      />
+      <div
+        className="d-flex align-items-center justify-content-center"
+        style={{ height: "100vh" }}
+      >
+        <div className="card border-0">
+          <div className="card-body">
+            <div className="text-center mb-3">
+              <img src="/nonpawiz.png" style={{ width: "50px" }} alt="" />
             </div>
-        </>
-    )
+            <div className="text-center mb-3">
+              <span className="text-main fw-normal">เข้าสู่ระบบ</span>
+            </div>
+            <div className="row">
+              <div className="col-md-12 mb-3">
+                <div className="form-floating mb-3">
+                  <input
+                    type="fname"
+                    className="form-control"
+                    id="name"
+                    placeholder="ชื่อผู้ใช้"
+                    onChange={(e) => setName(e.target.value)}
+                    value={name}
+                  />
+                  <label htmlFor="floatingInput">ชื่อผู้ใช้</label>
+                </div>
+              </div>
+              <div className="col-md-12 mb-3">
+                <div className="form-floating mb-3">
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="pass"
+                    placeholder="รหัสผ่าน"
+                    onChange={(e) => setPass(e.target.value)}
+                    value={pass}
+                  />
+                  <label htmlFor="floatingPassword">รหัสผ่าน</label>
+                </div>
+              </div>
+              <div className="col-md-12">
+                <button
+                  className="btn bg-main border-0 text-light p-2 fs-16 w-100 fw-normal"
+                  onClick={auth}
+                >
+                  เข้าสู่ระบบ
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Login
+export default Login;
--- src/views/chat/DemoChat.tsx
+++ src/views/chat/DemoChat.tsx
@@ -6,29 +6,32 @@ import { useParams } from "react-router-dom";
 import io from "socket.io-client";
 
 interface Chat {
-  id?: number | null | undefined,
-  me?: number | null | undefined,
-  message?: string | null | undefined,
-  room?: string | null | undefined,
+  id?: number | null | undefined;
+  me?: number | null | undefined;
+  message?: string | null | undefined;
+  room?: string | null | undefined;
 }
 
-function Friends() {
-
+function DemoChat() {
   const { to, from } = useParams();
   const me = from;
   const useSocket = io.connect("http://localhost:8000");
-  const [mymsg, SetMyMsg] = useState<Chat[]>([]);
-  const [tomsg, SetToMsg] = useState<Chat[]>([]);
   const [chat, SetChat] = useState<Chat[]>([]);
   const [message, setMessage] = useState("");
   const [room, setRoom] = useState(to);
 
   const appendMyMsg = (data: Chat) => {
-    if (data.me == me && data.message != '') {
-      SetChat((chat) => [...chat, { id: chat.length + 1, message: data.message, me: 1 }]);
+    if (data.me == me && data.message != "") {
+      SetChat((chat) => [
+        ...chat,
+        { id: chat.length + 1, message: data.message, me: 1 },
+      ]);
     } else {
-      if (data.message != '') {
-        SetChat((chat) => [...chat, { id: chat.length + 1, message: data.message, me: 0 }]);
+      if (data.message != "") {
+        SetChat((chat) => [
+          ...chat,
+          { id: chat.length + 1, message: data.message, me: 0 },
+        ]);
       }
     }
     setMessage("");
@@ -42,7 +45,19 @@ function Friends() {
     useSocket.emit("join_room", room);
     useSocket.emit("join_user", me);
     useSocket.emit("send_message", { me, message, room });
+    document.addEventListener("keydown", keyDownHandler);
+    return () => {
+      document.removeEventListener("keydown", keyDownHandler);
+    };
   }, []);
+
+  const keyDownHandler = (event: any) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      // 👇️ your logic here
+      document.getElementById("__send__")?.click();
+    }
+  };
 
   useEffect(() => {
     useSocket.on("receive_message", (data: Chat) => {
@@ -57,34 +72,31 @@ function Friends() {
       <NavBar />
       <Title to={to} />
       <Conversation chat={chat} />
-      <div className="d-flex justify-content-between align-items-center">
-        <input
-          id="textInputExample"
-          type="text"
-          className="form-control p-1 border-0 py-2 m-1"
-          placeholder="พิมพ์"
-          value={message}
-          onChange={(event) => {
-            setMessage(event.target.value);
-          }}
-        />
-        <div className="mx-1">
-          <button
-            className="btn bg-btn-ok text-light border-0 p-1 px-2 w-100"
-            onClick={sendMessage}
-          >
-            <i className="uil uil-navigator"></i>
-          </button>
-          {/* <button
-            className="btn bg-btn-ok text-light border-0 p-1 px-2 w-100"
-            onClick={() => console.log(mymsg)}
-          >
-            ดู
-          </button> */}
+      <div className="w-100" style={{ bottom: "0", position: "absolute" }}>
+        <div className="d-flex justify-content-between align-items-center">
+          <input
+            id="textInputExample"
+            type="text"
+            className="form-control p-1 border-0 py-2 m-1"
+            placeholder="พิมพ์"
+            value={message}
+            onChange={(event) => {
+              setMessage(event.target.value);
+            }}
+          />
+          <div className="mx-1">
+            <button
+              id="__send__"
+              className="btn bg-btn-ok text-light border-0 p-1 px-2 w-100"
+              onClick={sendMessage}
+            >
+              <i className="uil uil-navigator"></i>
+            </button>
+          </div>
         </div>
       </div>
     </>
   );
 }
 
-export default Friends;
+export default DemoChat;
